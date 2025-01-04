3d118d4d375abe5652de5f044dec8b63549dc519
Mali Mali <siwkrnesgyix1@gmail.com>
2025-01-04 18:24:56 +0700

    update

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,47 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 import axios from 'axios';
 
-const Home = () => {
+const Home = ({ username }) => {
   const [user, setUser] = useState(null); // เก็บข้อมูลผู้ใช้
-  const [username, setUsername] = useState(''); // Input Username
   const [errorMessage, setErrorMessage] = useState('');
 
-  const fetchUserData = async () => {
-    try {
-      const response = await axios.post('https://checkname-server.vercel.app/api/getUserData', { username });
-      if (response.data.success) {
-        setUser(response.data.user);
-        setErrorMessage('');
-      } else {
-        setErrorMessage(response.data.message || 'ไม่พบข้อมูลผู้ใช้');
+  useEffect(() => {
+    // ดึงข้อมูลผู้ใช้เมื่อ component โหลดครั้งแรก
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.post('https://checkname-server.vercel.app/api/login', { username });
+        if (response.data.success) {
+          setUser(response.data.user);
+          setErrorMessage('');
+        } else {
+          setErrorMessage(response.data.message || 'ไม่พบข้อมูลผู้ใช้');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        setErrorMessage('เกิดข้อผิดพลาดในการดึงข้อมูล');
       }
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-      setErrorMessage('เกิดข้อผิดพลาดในการดึงข้อมูล');
+    };
+
+    if (username) {
+      fetchUserData();
+    } else {
+      setErrorMessage('กรุณาเข้าสู่ระบบ');
     }
-  };
+  }, [username]);
 
   return (
     <div className="container">
       <div className="content">
-        <div>
-          <input
-            type="text"
-            placeholder="Enter username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <button onClick={fetchUserData}>ค้นหา</button>
-        </div>
-
         {user ? (
           <>
             <h3>{user.firstname} {user.lastname}</h3>
             <h3>{user.queue ? 'true : เข้าแถว' : 'false : ไม่เข้าแถว'}</h3>
           </>
         ) : (
-          <p>{errorMessage || 'โปรดค้นหาข้อมูลผู้ใช้'}</p>
+          <p>{errorMessage || 'กำลังโหลดข้อมูล...'}</p>
         )}
       </div>
     </div>
