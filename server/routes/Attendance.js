0d9cb4a8be5bc2f6a9477095e760bb383169fa39
Mali Mali <siwkrnesgyix1@gmail.com>
2025-01-27 13:36:07 +0700

    update

--- server/routes/Attendance.js
+++ server/routes/Attendance.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middleware/auth");
 const { listUsers, listname,deleteName} = require("../controllers/Attendance");
-const {receiveFaceData} = require("../controllers/receiveFaceData");
-const { receiveFaceData } = require("../controllers/faceRecognition");
+const {receiveFaceData} = require("../controllers/faceRecognition");
 
 router.post("/receiveFaceData", receiveFaceData); 
 router.get("/listname", listname); 
